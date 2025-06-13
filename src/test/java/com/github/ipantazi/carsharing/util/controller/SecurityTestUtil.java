1d0f992b543ab3fe9a3156356c12cb654e30871b
Illya Pantazi <illios.piv@gmail.com>
2025-06-13 13:39:28 +0200

    feat(security): add user role authority to CustomUserDetails and CustomUserDetailsService - add role and method authenticateTestUser() toSecurityTestUtil

--- src/test/java/com/github/ipantazi/carsharing/util/controller/SecurityTestUtil.java
+++ src/test/java/com/github/ipantazi/carsharing/util/controller/SecurityTestUtil.java
@@ -1,5 +1,7 @@
 package com.github.ipantazi.carsharing.util.controller;
 
+import static com.github.ipantazi.carsharing.util.TestDataUtil.createTestUser;
+
 import com.github.ipantazi.carsharing.model.User;
 import com.github.ipantazi.carsharing.security.CustomUserDetails;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,14 +19,25 @@ public class SecurityTestUtil {
                 user.getEmail(),
                 user.getPassword(),
                 user.getAuthorities(),
+                user.getRole(),
                 user.isEnabled()
         );
     }
 
     public static void setAuthenticationForUser(UserDetails userDetails) {
         Authentication authentication = new UsernamePasswordAuthenticationToken(
-                userDetails, null, userDetails.getAuthorities()
+                userDetails,
+                null,
+                userDetails.getAuthorities()
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);
     }
+
+    public static void authenticateTestUser(Long userId, User.Role role) {
+        User user = createTestUser(userId);
+        user.setRole(role);
+        CustomUserDetails customUserDetails = wrapAsUserDetails(user);
+        setAuthenticationForUser(customUserDetails);
+    }
+
 }
