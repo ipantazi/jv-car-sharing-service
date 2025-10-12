9394e3ed9978b846c421c38a0c3fa380702a9c3f
Illya Pantazi <illios.piv@gmail.com>
2025-10-13 00:16:35 +0200

    refactor(exception): remove StripeSessionExpiredException
    - Deleted unused StripeSessionExpiredException class

--- src/main/java/com/github/ipantazi/carsharing/exception/StripeSessionExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.github.ipantazi.carsharing.exception;
-
-public class StripeSessionExpiredException extends RuntimeException {
-    public StripeSessionExpiredException(String message) {
-        super(message);
-    }
-}
