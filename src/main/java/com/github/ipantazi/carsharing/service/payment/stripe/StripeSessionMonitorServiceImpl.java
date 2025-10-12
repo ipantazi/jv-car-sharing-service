d9252213f84e1110e393ad46fa9d2dead3f4b472
Illya Pantazi <illios.piv@gmail.com>
2025-10-12 15:10:07 +0200

    feat (task): enable scheduling and add Stripe session monitor service
    - Enabled scheduling in Application class using @EnableScheduling
    - Added StripeSessionMonitorServiceImpl to periodically check and expire outdated Stripe sessions
    - Implemented scheduled task running every 60 seconds to update payment statuses from PENDING to EXPIRED
    - Added error handling and logging for Stripe session verification

---
+++ src/main/java/com/github/ipantazi/carsharing/service/payment/stripe/StripeSessionMonitorServiceImpl.java
@@ -0,0 +1,47 @@
+package com.github.ipantazi.carsharing.service.payment.stripe;
+
+import com.github.ipantazi.carsharing.model.Payment;
+import com.github.ipantazi.carsharing.repository.payment.PaymentRepository;
+import com.stripe.exception.StripeException;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class StripeSessionMonitorServiceImpl implements StripeSessionMonitorService {
+    private final PaymentRepository paymentRepository;
+    private final StripeClient stripeClient;
+
+    @Override
+    @Scheduled(fixedRate = 60_000)
+    @Transactional
+    public void checkAndExpireSessions() {
+        Payment.Status pendingStatus = Payment.Status.PENDING;
+        Payment.Status expiredStatus = Payment.Status.EXPIRED;
+        List<Payment> pendingPayments = paymentRepository.findPaymentsByStatus(pendingStatus);
+
+        List<Payment> expiredPayments = new ArrayList<>();
+        for (Payment payment : pendingPayments) {
+            try {
+                if (stripeClient.isStripeSessionExpired(payment.getSessionId())) {
+                    payment.setStatus(expiredStatus);
+                    expiredPayments.add(payment);
+                }
+            } catch (StripeException e) {
+                log.error(
+                        "Stripe session check failed for session ID {}: {}",
+                        payment.getSessionId(),
+                        e.getMessage());
+            }
+        }
+        if (!expiredPayments.isEmpty()) {
+            paymentRepository.saveAll(expiredPayments);
+        }
+    }
+}
