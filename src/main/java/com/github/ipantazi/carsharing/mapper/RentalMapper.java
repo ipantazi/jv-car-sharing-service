2601cff68da202d558d8d915f339a4142c2e3264
Illya Pantazi <illios.piv@gmail.com>
2026-01-28 00:55:06 +0100

    refactor(mapper): align RentalMapper with entity associations

--- src/main/java/com/github/ipantazi/carsharing/mapper/RentalMapper.java
+++ src/main/java/com/github/ipantazi/carsharing/mapper/RentalMapper.java
@@ -4,9 +4,10 @@ import com.github.ipantazi.carsharing.config.MapperConfig;
 import com.github.ipantazi.carsharing.dto.car.CarDto;
 import com.github.ipantazi.carsharing.dto.enums.RentalStatus;
 import com.github.ipantazi.carsharing.dto.rental.RentalDetailedDto;
-import com.github.ipantazi.carsharing.dto.rental.RentalRequestDto;
 import com.github.ipantazi.carsharing.dto.rental.RentalResponseDto;
+import com.github.ipantazi.carsharing.model.Car;
 import com.github.ipantazi.carsharing.model.Rental;
+import com.github.ipantazi.carsharing.model.User;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import org.mapstruct.Mapper;
@@ -16,15 +17,20 @@ import org.mapstruct.Mapping;
 public interface RentalMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "actualReturnDate", ignore = true)
-    Rental toRentalEntity(RentalRequestDto rentalRequestDto, Long userId, LocalDate rentalDate);
+    Rental toRentalEntity(User user,
+                          Car car,
+                          LocalDate returnDate,
+                          LocalDate rentalDate);
 
     @Mapping(target = "id", source = "rental.id")
+    @Mapping(target = "userId", source = "rental.user.id")
     RentalResponseDto toRentalDto(Rental rental,
                                   CarDto carDto,
                                   RentalStatus status,
                                   BigDecimal baseRentalCost);
 
     @Mapping(target = "id", source = "rental.id")
+    @Mapping(target = "userId", source = "rental.user.id")
     RentalDetailedDto toRentalDetailedDto(Rental rental,
                                           CarDto carDto,
                                           BigDecimal baseRentalCost,
