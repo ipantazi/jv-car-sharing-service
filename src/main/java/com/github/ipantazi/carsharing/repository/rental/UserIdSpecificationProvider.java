91ea02b31686454f725154640d2b7c9d0d28dfb7
Illya Pantazi <illios.piv@gmail.com>
2026-01-28 01:24:40 +0100

    refactor(specification): align UserIdSpecificationProvider with Rental entity associations
    - Replaced direct 'userId' field access with navigation through 'user.id'
    - Introduced constants USER_FIELD and ID_FIELD for clarity and maintainability

--- src/main/java/com/github/ipantazi/carsharing/repository/rental/UserIdSpecificationProvider.java
+++ src/main/java/com/github/ipantazi/carsharing/repository/rental/UserIdSpecificationProvider.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserIdSpecificationProvider implements SpecificationProvider<Rental> {
     public static final String KEY = "userId";
+    private static final String USER_FIELD = "user";
+    private static final String ID_FIELD = "id";
 
     @Override
     public String getKey() {
@@ -18,7 +20,7 @@ public class UserIdSpecificationProvider implements SpecificationProvider<Rental
     public Specification<Rental> getSpecification(Object params) {
         if (params instanceof Long userId && userId > 0) {
             return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(
-                    root.get(this.getKey()), userId
+                    root.get(USER_FIELD).get(ID_FIELD), userId
             ));
         }
         return null;
